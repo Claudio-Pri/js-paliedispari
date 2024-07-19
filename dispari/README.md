1144733bb7f54a514bb685e04f8d2b0eed2d89af
Claudio-Pri <priv.claudio@gmail.com>
2024-07-19 17:25:56 +0200

    js dispari- scelta utente, inserisci numero, generazione numero computer

--- dispari/README.md
+++ dispari/README.md
@@ -1,1 +1,12 @@
-# template-html-css
+# JS-dispari
+L'utente sceglie pari o dispari 
+
+inserisce un numero da 1 a 5.
+
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+
+Sommiamo i due numeri
+
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+
+Dichiariamo chi ha vinto.
--- dispari/js/scripts.js
+++ dispari/js/scripts.js
@@ -1,9 +1,48 @@
-console.log('js collegato');
-
 //===== Appunti ======
 // ` backtick
 
 //===== Code ======
+// l'utente sceglie pari o dispari
+let userChoice = prompt('Pari o dispari?');
+// console.log('user choice', userChoice);
+// vorrei aggiungere solo 2 scelte magari con button
+// per ora controllo se la stringa corrisponde a pari o dispari
+if (userChoice == 'pari') {
+    userChoice = 'pari';
+    console.log('hai scelto pari')
+}
+else if (userChoice == 'dispari') {
+    userChoice = 'dispari';
+    console.log('hai scelto dispari');
+}
+else {
+    alert('scl');
+};
 
+// l'utente inserisce un numero da 1 a 5
+let userNumber = parseInt(prompt('Inserisci un numero da 1 a 5: '));
+if (isNaN(userNumber)) {
+    console.log('mannaggia a te');
+}
+else {
+    if (userNumber > 5 || userNumber < 1) {
+        console.log('mannaggia a te');
+    }
+    else {
+        console.log('il tuo numero è: ', userNumber);
+    }
+
+
+};
+// console.log(userNumber);
+//Genero un numero per il computer
+let compNumber = 0;
+compNumber = randomNumber();
+console.log('compNumber', compNumber);
 
 //===== Functions =====
+//funzione per generare il numero random
+function randomNumber() {
+    return Math.floor(Math.random() * (5 - 1 + 1)) + 1;
+};
+
